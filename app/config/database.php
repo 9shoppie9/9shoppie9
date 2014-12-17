437264d3a89e00828d6fddc2b1233c013b28f407
Murthy <murthyadapa27@gmail.com>
2014-12-18 01:24:06 +0530

    my first commit

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'hi_shoppie_db',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'murthy',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
